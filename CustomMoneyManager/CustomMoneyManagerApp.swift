d4a7be87861a6a0660be675885fb771ecd15d46a
唐智 <913422986@qq.com>
2026-05-18 10:12:24 +0800

    Initial Commit

---
+++ CustomMoneyManager/CustomMoneyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMoneyManagerApp.swift
+//  CustomMoneyManager
+//
+//  Created by windbylocus on 2026/5/18.
+//
+
+import SwiftUI
+
+@main
+struct CustomMoneyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
